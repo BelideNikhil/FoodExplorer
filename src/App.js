21dec1483bc840d5055787f9e22f1f0b1d7f8481
NikhilBelide <66310861+BelideNikhil@users.noreply.github.com>
2021-08-15 10:55:27 +0530

    add recipe,rating

--- src/App.js
+++ src/App.js
@@ -5,31 +5,52 @@ let foodDb = {
   Egg: [
     {
       recipeName: "Egg Maggi Masala",
-      recipeBy: "Sanjyot Keer"
+      recipeBy: "Sanjyot Keer",
+      rating: "5/5"
     },
     {
       recipeName: "Scrambled Eggs",
-      recipeBy: "Gordan Ramsay"
+      recipeBy: "Gordan Ramsay",
+      rating: "4.75/5"
+    },
+    {
+      recipeName: "Mushroom and Spinach Omelet",
+      recipeBy: "David Bonam",
+      rating: "4/5"
     }
   ],
   CheeseCake: [
     {
       recipeName: "Mango CheeseCake",
-      recipeBy: "Sanjyot Keer"
+      recipeBy: "Sanjyot Keer",
+      rating: "4.75/5"
+    },
+    {
+      recipeName: "Pomegranate CheeseCake",
+      recipeBy: "Ranveer Brar",
+      rating: "4/5"
     },
     {
       recipeName: "Strawberry CheeseCake",
-      recipeBy: "Kunal Kapoor"
+      recipeBy: "Kunal Kapoor",
+      rating: "3/5"
     }
   ],
   Bread: [
     {
       recipeName: "Garlic Bread",
-      recipeBy: "Sanjyot Keer"
+      recipeBy: "Sanjyot Keer",
+      rating: "4/5"
+    },
+    {
+      recipeName: "Mumbai Veg Grill Sandwich",
+      recipeBy: "Sanjyot Keer",
+      rating: "4/5"
     },
     {
       recipeName: "Veg Lasangna",
-      recipeBy: "The Terrace Kitchen"
+      recipeBy: "The Terrace Kitchen",
+      rating: "3.5/5"
     }
   ]
 };
@@ -66,6 +87,7 @@ export default function App() {
                 {/* getting value by .key */}
                 <li className="name">{each.recipeName}</li>
                 <li className="sub">{each.recipeBy}</li>
+                <li className="sub">{each.rating}</li>
               </ul>
             );
           })}
